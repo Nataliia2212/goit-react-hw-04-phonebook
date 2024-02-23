5af128027ad1d252ded8dd45613155aa6686ce29
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-02-23 20:14:17 +0200

    finish

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import ContactItem from 'components/ContactItem/ContactItem';
-import css from './ContactsList.module.css'
+import css from './ContactsList.module.css';
 
-export default function ContactsList({contacts, onDelete}) {
+export default function ContactsList({ contacts, onDelete }) {
   return (
-   
-        
-        <ul className={css.list}>
-        {contacts.map((user) => (
-				<ContactItem key={user.id} {...user} onDelete={onDelete}/>
-			))}
-        </ul>
-  )
+    <ul className={css.list}>
+      {contacts.map(user => (
+        <ContactItem key={user.id} {...user} onDelete={onDelete} />
+      ))}
+    </ul>
+  );
 }
-
